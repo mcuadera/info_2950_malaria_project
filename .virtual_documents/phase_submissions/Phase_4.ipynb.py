a65b7bc39a1bf85ce05f15daaa1a58a28ed72e89
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2021-04-18 22:37:44 -0400

    phase 4 updates

--- .virtual_documents/phase_submissions/Phase_4.ipynb.py
+++ .virtual_documents/phase_submissions/Phase_4.ipynb.py
@@ -1,6 +1,3 @@
-
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,8 +11,111 @@ plt.rcParams["figure.figsize"] = (10, 5)
 
 curated_df_url = "https://raw.githubusercontent.com/mcuadera/info_2950_malaria_project/main/datasets/malaria_project_curated_data.csv"
 malaria_df = pd.read_csv(curated_df_url)
-malaria_df = malaria_df.set_index('Year')
+malaria_df = malaria_df.set_index("Year")
 malaria_df.head()
+
+
+by_year = malaria_df.groupby("Year")
+
+
+plt.plot(by_year["Deaths"].mean(), label="Mean")
+plt.plot(by_year["Deaths"].median(), label="Median")
+plt.title("Deaths per Year")
+plt.xlabel("Year")
+plt.xticks(rotation = 40)
+plt.ylabel("Deaths")
+plt.legend()
+plt.show()
+
+
+plt.plot(by_year["Deaths"].median(), color="orange")
+plt.title("Median Deaths per Year")
+plt.xlabel("Year")
+plt.xticks(rotation = 40)
+plt.ylabel("Median Deaths")
+plt.show()
+
+
+by_region = malaria_df.groupby(["Region","Year"])
+
+
+by_region.mean()["Deaths"].unstack().plot(kind="bar")
+plt.title("Average Deaths per Region (2000-2013)")
+plt.xlabel("Region")
+plt.xticks(rotation=40)
+plt.ylabel("Average Deaths")
+plt.legend(loc='upper center', bbox_to_anchor=(1.5, 0.8), shadow=True, ncol=4)
+plt.show()
+
+
+by_region.median()["Deaths"].unstack().plot(kind="bar")
+plt.title("Median Deaths per Region (2000-2013)")
+plt.xlabel("Region")
+plt.xticks()
+plt.ylabel("Median Deaths")
+plt.legend(loc='upper center', bbox_to_anchor=(1.5, 0.8), shadow=True, ncol=4)
+plt.show()
+
+
+africa = malaria_df[malaria_df["Region"]=="Africa"]
+
+
+africa_2013 = africa.loc["2013-01-01"]
+plt.bar(africa_2013["Country"], africa_2013["Deaths"])
+plt.title("Total Deaths per African Country 2013")
+plt.xlabel("Country")
+plt.xticks(rotation=90)
+plt.ylabel("Total Deaths")
+plt.show()
+
+
+africa_2000 = africa[africa["Year"]==2000]
+plt.bar(africa_2000["Country"], africa_2000["Deaths"])
+plt.title("Total Deaths per African Country 2000")
+plt.xlabel("Country")
+plt.xticks(rotation=90)
+plt.ylabel("Total Deaths")
+plt.show()
+
+
+angola = africa[africa["Country"]=="Angola"]
+kenya = africa[africa["Country"]=="Kenya"]
+plt.plot(angola["Year"], angola["Deaths"], label="Angola")
+plt.plot(kenya["Year"], kenya["Deaths"], label="Kenya")
+plt.title("Deaths per Year")
+plt.xlabel("Year")
+plt.xticks(rotation = 40)
+plt.ylabel("Deaths")
+plt.legend()
+plt.show()
+
+
+year_region = malaria_df.groupby(["Year","Region"])
+year_region["Incidence"].mean().unstack().plot()
+plt.title("Average Incidence Per Year")
+plt.xlabel("Year")
+plt.ylabel("Incidence (cases per 1000 people)")
+plt.show()
+
+
+year_region = malaria_df.groupby(["Year","Region"])
+year_region["Incidence"].median().unstack().plot()
+plt.title("Median Incidence Per Year")
+plt.xlabel("Year")
+plt.ylabel("Incidence (cases per 1000 people)")
+plt.show()
+
+
+malaria_corr_matrix = malaria_df.corr()
+sns.heatmap(malaria_corr_matrix, cmap="YlGnBu", annot=True)
+plt.show()
+
+
+plt.scatter(malaria_df["Confirmed Cases"], malaria_df['Deaths'])
+plt.title("Malaria Deaths vs Confirmed Cases of Malaria")
+plt.xlabel("Confirmed Cases")
+plt.ylabel("Deaths")
+plt.show()
 
 
 #Generating dummy variables for regions
@@ -74,6 +174,9 @@ for i in range(len(incidence_model_pooled_coeff)):
 
 
 
+
+
+    
 
 
 
